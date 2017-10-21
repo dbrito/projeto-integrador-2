7ea75e7ecc8a2fbdf236ccb46bebbb7fa6e33a2b
willeneuve.sassuncao <willeneuve.sassuncao@CAS527216W0710.senacsp.edu.br>
2017-10-20 22:16:11 -0200

    testando commit pelo netbeans

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,14 @@
+
+package javaapplication2;
+
+
+public class JavaApplication2 {
+
+  
+    public static void main(String[] args) {
+        
+        System.out.println("test");
+     
+    }
+    
+}
